834f2873eddefd9038de88c7cf021d2fc7fcd32e
guiiiz <127798968+guiiiz@users.noreply.github.com>
2023-05-15 22:17:24 -0300

    Update Média Sala.cpp

--- Média Sala.cpp
+++ Média Sala.cpp
@@ -3,29 +3,20 @@ using namespace std;
 
 int main() {
     // Definindo a quantidade de alunos em cada sala
-    int sala1 = 35, sala2 = 4, sala3 = 22, sala4 = 20, sala5 = 36, sala6 = 30;
+    int salas[6][2] = {{1, 35}, {2, 4}, {3, 22}, {4, 20}, {5, 36}, {6, 30}};
     // Calculando a média
-    float media = (sala1 + sala2 + sala3 + sala4 + sala5 + sala6) / 6.0;
+    float media = 0;
+    for (int i = 0; i < 6; i++) {
+        media += salas[i][1];
+    }
+    media /= 6.0;
     
     // Verificando quais salas possuem quantidade de alunos acima da média e apresentando o resultado
-    if (sala1 > media) {
-        cout << "Sala 1 tem uma quantidade de pessoas acima da média." << endl;
-    }
-    if (sala2 > media) {
-        cout << "Sala 2 tem uma quantidade de pessoas acima da média." << endl;
-    }
-    if (sala3 > media) {
-        cout << "Sala 3 tem uma quantidade de pessoas acima da média." << endl;
-    }
-    if (sala4 > media) {
-        cout << "Sala 4 tem uma quantidade de pessoas acima da média." << endl;
-    }
-    if (sala5 > media) {
-        cout << "Sala 5 tem uma quantidade de pessoas acima da média." << endl;
-    }
-    if (sala6 > media) {
-        cout << "Sala 6 tem uma quantidade de pessoas acima da média." << endl;
+    for (int i = 0; i < 6; i++) {
+        if (salas[i][1] > media) {
+            cout << "Sala " << salas[i][0] << " tem uma quantidade de pessoas acima da média." << endl;
+        }
     }
     
     return 0;
-}
+} 
